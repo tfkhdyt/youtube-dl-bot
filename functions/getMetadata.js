1e08b5ab2412dd027b0595311c39a5929b8bf0fc
tfkhdyt <ta142fa@gmail.com>
2021-09-21 17:02:54 +0700

    modulify getMetadata

---
+++ functions/getMetadata.js
@@ -0,0 +1,15 @@
+module.exports = (link, ctx) => {
+  return youtubedl(link, {
+    dumpSingleJson: true,
+    noWarnings: true,
+    noCallHome: true,
+    noCheckCertificate: true,
+    preferFreeFormats: true,
+    youtubeSkipDashManifest: true
+  })
+  .then(data => data)
+  .catch(err => {
+    ctx.deleteMessage(textLoad);
+    return ctx.reply('Video tidak ditemukan, pastikan link video tersebut sudah benar! 🙏🏼');
+  });
+};
