4dd7378d63bbf34c673d2b1b350425ec8a5da754
tfkhdyt <ta142fa@gmail.com>
2021-09-21 15:59:59 +0700

    ubah 500MB jadi 450MB

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ const showQuality = formats => {
 // command start
 bot.start((ctx) => ctx.replyWithMarkdown(`Halo ${ctx.from.first_name}, selamat datang di YT-DL Bot, kirim link video yang ingin anda unduh untuk mengunduh video tersebut.
 
-*PERHATIAN*: Dikarenakan storage hosting yang terbatas, maka kalian tidak dapat mengunduh video yang memiliki ukuran di atas *500 MB*`));
+*PERHATIAN*: Dikarenakan storage hosting yang terbatas, maka kalian tidak dapat mengunduh video yang memiliki ukuran di atas *450 MB*`));
 
 // command help
 bot.command('help', (ctx) => ctx.reply(`Anda hanya perlu mengirimkan link dari video yang ingin diunduh`));
