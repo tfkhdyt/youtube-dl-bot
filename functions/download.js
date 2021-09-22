1c1ffade8b586c61052e0f6b466d98bb96d35dbb
tfkhdyt <ta142fa@gmail.com>
2021-09-22 08:08:23 +0700

    ubah c menjadi continue di download.js

--- functions/download.js
+++ functions/download.js
@@ -12,8 +12,7 @@ module.exports = (url, formatCode, ctx, info) => {
     format: `${formatCode}+140`,
     mergeOutputFormat: 'mp4',
     ignoreErrors: true,
-    c: true,
-    verbose: true,
+    continue_: true,
     output: `%(id)s-${formatCode}`,
     ffmpegLocation: "node_modules/ffmpeg-static/ffmpeg"
   })
