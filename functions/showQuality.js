322517bd0abc0f7de59b57a4beeb7966115afd3e
tfkhdyt <ta142fa@gmail.com>
2021-09-25 09:07:04 +0700

    ubah AVC1 jadi AVC, dan AV01 jadi AV1

--- functions/showQuality.js
+++ functions/showQuality.js
@@ -5,7 +5,15 @@ module.exports = (formats, audioFileSize) => {
   const keyCallback = formats.map((format) => {
     const id = format.format_id;
     const quality = format.format_note;
-    const vcodec = format.vcodec.substring(0, 4).toUpperCase();
+    let vcodec = format.vcodec.substring(0, 4).toUpperCase();
+    switch (vcodec) {
+      case 'AVC1':
+        vcodec = 'AVC';
+        break;
+      case 'AV01':
+        vcodec = 'AV1';
+        break;
+    }
     const fileSize = formatBytes(format.filesize + audioFileSize);
     return Key.callback(`${quality} | ${vcodec} | ${fileSize}`, id);
   });
